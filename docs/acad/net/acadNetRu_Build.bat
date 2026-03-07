b57f57af1285a5f107449f726aaa2065b6a5556f
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2026-03-07 13:44:47 +0300

    edit build script
    Добавление авто-копирования html в web-версию справочника

--- docs/acad/net/acadNetRu_Build.bat
+++ docs/acad/net/acadNetRu_Build.bat
@@ -1,2 +1,12 @@
 mdbook build
+
+REM Копирование в C:\Users\Georg\Documents\GitHub\acadDevDocsRu_Web\acadNetDeveloperGuideRu
+:: перед использованием у себя скопируйте себе локально репозторий acadDevDocsRu_Web или удалить вообще эти действия
+:: Удаление папки и создание заново, чтобы перезаписать начисто. Одноименные файлы и стили те же. Просто на всякий случай 
+rmdir /s /q "..\..\..\..\acadDevDocsRu_Web\acadNetDeveloperGuideRu"
+mkdir "..\..\..\..\acadDevDocsRu_Web\acadNetDeveloperGuideRu"
+
+xcopy book "..\..\..\..\acadDevDocsRu_Web\acadNetDeveloperGuideRu" /Y /I /E
+
+:: Открыть справку в браузере
 explorer "book\index.html"
